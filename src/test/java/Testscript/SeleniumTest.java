3b866600f0d2b56daf27674ffe6a212884d9024e
amarpunnam <amarendar1000@gmail.com>
2023-10-04 05:58:54 +0000

    project modifed

--- src/test/java/Testscript/SeleniumTest.java
+++ src/test/java/Testscript/SeleniumTest.java
@@ -1,5 +1,11 @@
 package Testscript;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class SeleniumTest {
+	public static void main(String[] args) {
+		WebDriver driver = new ChromeDriver();
+	}
 
 }
